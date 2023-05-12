575200bb14f5552229a0846c9b4a3f6ea91f0a46
ali mirzajanpoor <128279744+Alimirzajanpoor@users.noreply.github.com>
2023-05-12 14:00:26 +0330

    Update otp_verfify.py

--- otp_verfify.py
+++ otp_verfify.py
@@ -1,30 +1,33 @@
 from sqlalchemy.orm import Session
 import models,schemas,sendmsg
+from sendmsg import send_text
 from fastapi import HTTPException,status
 
 import random
-def generate_otp():
+def generate_otp(telegram_id):
     global rdm
     rdm=random.randint(1000,5000)
+    send_text(telegram_id,"Generated OTP"+str(rdm))
     return rdm
+
 def verify_otp(otp:int,db: Session,userid: int):
     body=db.query(models.Otp).filter(models.Otp.user_id==userid)
-
     if body.first()==None:
-        try:
-            if otp==rdm:
 
-                Verified=models.Otp(telegram_otp='Verified',user_id=userid)
+        try:
+            if otp == rdm:
+                Verified = models.Otp(telegram_otp='Verified', user_id=userid)
                 db.add(Verified)
                 db.commit()
                 db.refresh(Verified)
-                raise HTTPException(detail="Verified",status_code=status.HTTP_200_OK)
-              
-            else:
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Wrong otp")
+                raise HTTPException(status_code=status.HTTP_200_OK, detail="OTP verified")
+        except NameError:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="OTP not generated yet")
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Wrong OTP")
+        
 
-        except:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Wrong otp")
+    
         
     for i in body:
         if i.__dict__['telegram_otp']=='Verified':
